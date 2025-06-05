5449b34f95d0ba039ecf4d62617250a998ca168b
notpr <notprovided64@gmail.com>
2025-06-04 20:08:02 -0700

    started asg5

---
+++ asg5/index.js
@@ -0,0 +1,140 @@
+import * as THREE from 'three';
+
+function addHtmlActions() {
+  //document
+  //  .getElementById("lightSlideX")
+  //  .addEventListener("mousemove", function (ev) {
+  //    if (ev.buttons == 1) {
+  //      console.log("setting slidex");
+  //      g_lightPos[0] = this.value / 100;
+  //    }
+  //  });
+}
+
+function main() {
+
+  // canvas and renderer
+	const canvas = document.querySelector( '#c' );
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+
+
+  // camera and resize event
+	const fov = 75;
+	const aspect = 2;
+	const near = 0.1;
+	const far = 5;
+	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+	camera.position.z = 2;
+
+  window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  });
+
+
+  // scene + textures
+	const scene = new THREE.Scene();
+  const loader = new THREE.TextureLoader();
+
+	{
+
+		const color = 0xFFFFFF;
+		const intensity = 3;
+		const light = new THREE.DirectionalLight( color, intensity );
+		light.position.set( - 1, 2, 4 );
+		scene.add( light );
+
+	}
+
+	const boxWidth = 1;
+	const boxHeight = 1;
+	const boxDepth = 1;
+	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+
+  const texture = loader.load('resources/floor.jpg', () => {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+
+    texture.repeat.set(100, 100);
+  });
+
+  // make plane w texture
+  const material = new THREE.MeshStandardMaterial({
+    map: texture,
+    side: THREE.DoubleSide,
+  });
+
+  // Create geometry and mesh
+  const geometry = new THREE.PlaneGeometry(10000, 10000);
+  const plane = new THREE.Mesh(geometry, material);
+  plane.rotation.x = -Math.PI / 2;
+  plane.receiveShadow = true;
+
+  scene.add(plane);
+
+	function makeInstance( geometry, color, x ) {
+
+		const material = new THREE.MeshPhongMaterial( { color } );
+
+		const cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+
+		cube.position.x = x;
+
+		return cube;
+
+	}
+
+	const cubes = [
+		makeInstance( geometry, 0x44aa88, 0 ),
+		makeInstance( geometry, 0x8844aa, - 2 ),
+		makeInstance( geometry, 0xaa8844, 2 ),
+	];
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+	function render( time ) {
+
+		time *= 0.001; // convert time to seconds
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+		cubes.forEach( ( cube, ndx ) => {
+
+			const speed = 1 + ndx * .1;
+			const rot = time * speed;
+			cube.rotation.x = rot;
+			cube.rotation.y = rot;
+
+		} );
+
+		renderer.render( scene, camera );
+
+		requestAnimationFrame( render );
+
+	}
+
+	requestAnimationFrame( render );
+
+}
+
+main();
