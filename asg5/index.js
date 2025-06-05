8e542f5017fd26d74944a995aea717772a7f5168
fake <fake@fake.com>
2025-06-04 21:04:47 -0700

    plane working

--- asg5/index.js
+++ asg5/index.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 function addHtmlActions() {
   //document
@@ -12,23 +12,21 @@ function addHtmlActions() {
 }
 
 function main() {
-
   // canvas and renderer
-	const canvas = document.querySelector( '#c' );
+  const canvas = document.querySelector("#c");
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
-
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
   // camera and resize event
-	const fov = 75;
-	const aspect = 2;
-	const near = 0.1;
-	const far = 5;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 2;
+  const fov = 75;
+  const aspect = 2;
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
 
   window.addEventListener("resize", () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -37,65 +35,62 @@ function main() {
     camera.updateProjectionMatrix();
   });
 
-
   // scene + textures
-	const scene = new THREE.Scene();
+  const scene = new THREE.Scene();
   const loader = new THREE.TextureLoader();
 
-	{
+  {
+    const color = 0xffffff;
+    const intensity = 3;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
 
-		const color = 0xFFFFFF;
-		const intensity = 3;
-		const light = new THREE.DirectionalLight( color, intensity );
-		light.position.set( - 1, 2, 4 );
-		scene.add( light );
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-	}
-
-	const boxWidth = 1;
-	const boxHeight = 1;
-	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
-
-  const texture = loader.load('resources/floor.jpg', () => {
+  const texture = loader.load("resources/floor.jpg", () => {
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
 
-    texture.repeat.set(100, 100);
+    texture.repeat.set(10000, 10000);
   });
 
   // make plane w texture
-  const material = new THREE.MeshStandardMaterial({
-    map: texture,
-    side: THREE.DoubleSide,
-  });
+  {
+    const material = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    });
 
-  // Create geometry and mesh
-  const geometry = new THREE.PlaneGeometry(10000, 10000);
-  const plane = new THREE.Mesh(geometry, material);
-  plane.rotation.x = -Math.PI / 2;
-  plane.receiveShadow = true;
+    // Create geometry and mesh
+    const geometry = new THREE.PlaneGeometry(10000, 10000);
+    const plane = new THREE.Mesh(geometry, material);
+    plane.rotation.x = -Math.PI / 2;
+    plane.position.y = -1;
+    plane.receiveShadow = true;
+    scene.add(plane);
+  }
 
-  scene.add(plane);
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({ color });
 
-	function makeInstance( geometry, color, x ) {
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-		const material = new THREE.MeshPhongMaterial( { color } );
+    cube.position.x = x;
 
-		const cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
+    return cube;
+  }
 
-		cube.position.x = x;
-
-		return cube;
-
-	}
-
-	const cubes = [
-		makeInstance( geometry, 0x44aa88, 0 ),
-		makeInstance( geometry, 0x8844aa, - 2 ),
-		makeInstance( geometry, 0xaa8844, 2 ),
-	];
+  const cubes = [
+    makeInstance(geometry, 0x44aa88, 0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844, 2),
+  ];
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -108,9 +103,8 @@ function main() {
     return needResize;
   }
 
-	function render( time ) {
-
-		time *= 0.001; // convert time to seconds
+  function render(time) {
+    time *= 0.001; // convert time to seconds
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
@@ -118,23 +112,19 @@ function main() {
       camera.updateProjectionMatrix();
     }
 
-		cubes.forEach( ( cube, ndx ) => {
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * 0.1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
 
-			const speed = 1 + ndx * .1;
-			const rot = time * speed;
-			cube.rotation.x = rot;
-			cube.rotation.y = rot;
+    renderer.render(scene, camera);
 
-		} );
+    requestAnimationFrame(render);
+  }
 
-		renderer.render( scene, camera );
-
-		requestAnimationFrame( render );
-
-	}
-
-	requestAnimationFrame( render );
-
+  requestAnimationFrame(render);
 }
 
 main();
